65e23134d6563138bd0c9399a1e9f605fdc0a258
finn-01 <nguyensam576@gmail.com>
2022-02-20 01:14:24 +0700

    done

---
+++ src/main/java/vn/finn/spring/configproperties/AppProperties.java
@@ -0,0 +1,15 @@
+package vn.finn.spring.configproperties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Data // Lombok, xem chi tiết tại bài viết
+@Component // Là 1 spring bean
+// @PropertySource("classpath:loda.yml") // Đánh dấu để lấy config từ trong file loda.yml
+@ConfigurationProperties(prefix = "loda") // Chỉ lấy các config có tiền tố là "loda"
+public class AppProperties {
+    private String email;
+    private String googleAnalyticsId;
+}
--- src/main/java/vn/finn/spring/configproperties/ConfigPropertiesApplication.java
+++ src/main/java/vn/finn/spring/configproperties/ConfigPropertiesApplication.java
@@ -1,13 +1,27 @@
 package vn.finn.spring.configproperties;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-public class ConfigPropertiesApplication {
+@EnableConfigurationProperties
+public class ConfigPropertiesApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigPropertiesApplication.class, args);
-    }
 
+
+    }
+    @Autowired
+    AppProperties appProperties;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Global variable:");
+        System.out.println("\t Email: "+appProperties.getEmail());
+        System.out.println("\t GA ID: "+appProperties.getGoogleAnalyticsId());
+    }
 }
