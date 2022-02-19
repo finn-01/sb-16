fc615e9843e19ff376d5ab8503bbd7b6733af692
finn-01 <nguyensam576@gmail.com>
2022-02-20 00:44:18 +0700

    set up

---
+++ src/main/java/vn/finn/spring/configproperties/ConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.configproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigPropertiesApplication.class, args);
+    }
+
+}
